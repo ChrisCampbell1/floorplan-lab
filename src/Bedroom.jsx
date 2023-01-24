91f89806060e61d457b0bc561184a6a74a73bd2b
Chris Campbell <christopherecampbell@gmail.com>
2023-01-23 19:34:27 -0700

    add kitchen oven and sink components

---
+++ src/Bedroom.jsx
@@ -0,0 +1,9 @@
+const Bedroom = (props) => {
+  return ( 
+    <div>
+      <h3>this is a bedroom</h3>
+    </div>
+  )
+}
+
+export default Bedroom;
---
+++ src/FloorPlan.jsx
@@ -0,0 +1,14 @@
+import Bedroom from "./Bedroom";
+import Kitchen from "./Kitchen";
+
+const FloorPlan = (params) => {
+  return ( 
+    <div>
+      <h3>This is a floorplan comp</h3>
+      <Bedroom />
+      <Kitchen />
+    </div>
+  )
+}
+
+export default FloorPlan;
