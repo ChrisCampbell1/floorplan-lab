46f483b6f395419845cae3dff544141527b7473c
Chris Campbell <christopherecampbell@gmail.com>
2023-01-23 19:45:37 -0700

    move components from floorplan.jsx into app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import FloorPlan from './FloorPlan';
+import './App.css'
+import Bedroom from './Bedroom'
+import Kitchen from './Kitchen'
+import Bath from './Bath'
+import LivingRoom from './LivingRoom'
 
 function App() {
   return (
     <div>
-      <FloorPlan />
+      <Bedroom bedNum={1}/>
+      <Kitchen />
+      <Bath size={"Full"}/>
+      <Bedroom bedNum={2}/>
+      <LivingRoom />
+      <Bath size={"Half"}/>
+      <Bedroom bedNum={3}/>
     </div>
   );
 }
